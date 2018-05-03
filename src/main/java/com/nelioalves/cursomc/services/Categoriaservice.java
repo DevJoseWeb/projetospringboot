cfd2553afcfb599127c027c3919623849a8d28f5
deleaolucas@gmail.com <lucas.leao@rcadigital.com.br>
2018-05-03 16:29:30 -0300

    comentários das alterações

---
+++ src/main/java/com/nelioalves/cursomc/services/Categoriaservice.java
@@ -0,0 +1,22 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Categoria;
+import com.nelioalves.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class Categoriaservice {
+	
+	@Autowired
+	private CategoriaRepository repository;
+	public Optional<Categoria> buscar(Integer id) {
+		Optional<Categoria> obj = repository.findById(id);
+		
+		return obj;
+	}
+
+}
