755c6f5461f6bbef1350504a41ab1b5fbb6228af
deleaolucas@gmail.com <lucas.leao@rcadigital.com.br>
2018-05-24 15:59:40 -0300

    alteração

--- src/main/java/com/nelioalves/cursomc/services/ClienteService.java
+++ src/main/java/com/nelioalves/cursomc/services/ClienteService.java
@@ -204,7 +204,6 @@ public class ClienteService {
 	}
 
 	public boolean verificaCnpj(Cliente obj) {
-		// considera-se erro CNPJ's formados por uma sequencia de numeros iguais
 		if (obj.getCpfOuCnpj().equals("00000000000000") || obj.getCpfOuCnpj().equals("11111111111111")
 				|| obj.getCpfOuCnpj().equals("22222222222222") || obj.getCpfOuCnpj().equals("33333333333333")
 				|| obj.getCpfOuCnpj().equals("44444444444444") || obj.getCpfOuCnpj().equals("55555555555555")
@@ -216,15 +215,11 @@ public class ClienteService {
 		char dig13, dig14;
 		int sm, i, r, num, peso;
 
-		// "try" - protege o código para eventuais erros de conversao de tipo (int)
 		try {
-			// Calculo do 1o. Digito Verificador
 			sm = 0;
 			peso = 2;
+			
 			for (i = 11; i >= 0; i--) {
-				// converte o i-ésimo caractere do CNPJ em um número:
-				// por exemplo, transforma o caractere '0' no inteiro 0
-				// (48 eh a posição de '0' na tabela ASCII)
 				num = (int) (obj.getCpfOuCnpj().charAt(i) - 48);
 				sm = sm + (num * peso);
 				peso = peso + 1;
@@ -238,7 +233,6 @@ public class ClienteService {
 			else
 				dig13 = (char) ((11 - r) + 48);
 
-			// Calculo do 2o. Digito Verificador
 			sm = 0;
 			peso = 2;
 			for (i = 12; i >= 0; i--) {
@@ -255,7 +249,6 @@ public class ClienteService {
 			else
 				dig14 = (char) ((11 - r) + 48);
 
-			// Verifica se os dígitos calculados conferem com os dígitos informados.
 			if ((dig13 == obj.getCpfOuCnpj().charAt(12)) && (dig14 == obj.getCpfOuCnpj().charAt(13)))
 				return (true);
 			else
